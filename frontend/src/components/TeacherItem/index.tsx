5b234d6f459639506c680720fdb07acd641cf202
Fellipe <fellipes41@gmail.com>
2021-01-10 18:54:46 -0300

    up

---
+++ frontend/src/components/TeacherItem/index.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+
+import WhatappIcon from '../../assets/images/icons/whatsapp.svg';
+import api from '../../services/api';
+
+import './styles.css';
+
+export interface Teacher {
+    id: number;
+    avatar: string;
+    bio: string;
+    cost: string;
+    name: string;
+    subject: string;
+    whatsapp: string
+}
+
+interface TeacherItemProps {
+    teacher: Teacher
+}
+
+const TeacherItem: React.FC<TeacherItemProps> = ({ teacher }) => {
+
+    function createNewConnection() {
+        api.post('/connections', {
+            user_id: teacher.id,
+        }).then((res) => {
+            console.log('Enviado com sucesso');
+        }).catch((err) => {
+            console.log('err', err);
+        })
+    }
+
+    return (
+        <article className="teacher-item">
+            <header>
+                <img src={teacher.avatar} alt={teacher.name} />
+                <div>
+                    <strong>{teacher.name}</strong>
+                    <span>{teacher.subject}</span>
+                </div>
+            </header>
+
+            <p>{teacher.bio}</p>
+
+            <footer>
+                <p>
+                    Preço/hora
+                    <strong>R$ {teacher.cost}</strong>
+                </p>
+                <a
+                    onClick={createNewConnection}
+                    href={`https://wa.me/${teacher.whatsapp}`}
+                    target="_blank"
+                >
+                    <img src={WhatappIcon} alt="Whatapp" />
+                        Entrar em contato
+                </a>
+            </footer>
+        </article>
+    )
+}
+
+export default TeacherItem;
+
